712a2aecd2f39f42ab816afaf4a2fd6d000d5768
trongduc2206 <trongduc22062000@gmail.com>
2021-06-23 14:59:18 +0700

    change laptop commit

--- src/MapScreen.js
+++ src/MapScreen.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect,useMemo, useCallback} from 'react';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 import {Layout, Menu, Breadcrumb, Input, Button, Modal, Table, Form, Checkbox} from 'antd';
@@ -7,19 +7,20 @@ import {GithubOutlined} from "@ant-design/icons/lib";
 import {connect} from 'react-redux';
 import vector from './components/human.png'
 import {BrowserRouter, Route, useHistory} from "react-router-dom";
+
 const {Header, Content, Footer} = Layout;
 const {Search} = Input;
 
 export function MapScreen(props) {
     const [visible, setVisible] = useState(false)
     const [loginVisible, setLoginVisible] = useState(true)
-    const [windowVisible, setWindowVisible]= useState(false)
+    const [windowVisible, setWindowVisible] = useState(false)
     const [marker, setMarker] = useState()
     const [place, setPlace] = useState("")
-    const [currentLocation, setCurrentLocation]= useState({lat:21.005492, long:105.844257})
-    const [name, setName]= useState("")
-    const [location, setLocation] = useState({lat:21.005492, long:105.844257})
-    const {objects, getObjects, userName, resetLoginStatus}= props
+    const [currentLocation, setCurrentLocation] = useState({lat: 21.005492, long: 105.844257})
+    const [name, setName] = useState("")
+    const [location, setLocation] = useState({lat: 21.005492, long: 105.844257})
+    const {objects, getObjects, userName, resetLoginStatus} = props
     const layout = {
         labelCol: {
             span: 8,
@@ -46,10 +47,10 @@ export function MapScreen(props) {
         setMarker(marker);
         setPlace(props)
     }
-    const onSearchHandle = (v,e)=>{
+    const onSearchHandle = (v, e) => {
         console.log(v);
         console.log(currentLocation.lat, currentLocation.long);
-        getObjects(v,currentLocation.lat, currentLocation.long);
+        getObjects(v, currentLocation.lat, currentLocation.long);
         // if(objects!==null&&objects[0]!=null) {
         //     console.log(objects[0]);
         //     setLocation({lat: objects[0].lat, long: objects[0].long})
@@ -78,73 +79,85 @@ export function MapScreen(props) {
             longitude: 105.830335
         },
     ]
-    const listLocation = listLongLat.map((g)=><Marker position={{lat: g.latitude, lng: g.longitude}}/>)
+    const listLocation = listLongLat.map((g) => <Marker position={{lat: g.latitude, lng: g.longitude}}/>)
 
-    const tableColumn =[
-        {title: "Name",
-            dataIndex:"name"
+    const tableColumn = [
+        {
+            title: "Name",
+            dataIndex: "name"
         },
         {
-            title:"Action",
+            title: "Action",
             render: (record) => {
-                return  <Button onClick={()=>{
-                    console.log('record',record)
+                return <Button onClick={() => {
+                    console.log('record', record)
                     console.log('record.latitude', record.latitude)
                     console.log('record.longitude', record.longitude)
-                    setLocation({lat:record.latitude, long: record.longitude})
+                    setLocation({lat: record.latitude, long: record.longitude})
                     setVisible(false)
                     setName(record.name)
-                }} >Select</Button>
+                }}>Select</Button>
             }
         }
     ]
-    const handleOK =() =>{
+    const handleOK = () => {
         setVisible(false)
     }
-    const handleCancel=()=>{
+    const handleCancel = () => {
         setVisible(false)
     }
     let history = useHistory();
 
-    const onLogOutHandle=()=>{
+    const onLogOutHandle = () => {
         resetLoginStatus()
         history.push("/login")
 
     }
-    useEffect(()=>{
-        console.log('location',location)
-        console.log('location lat',location.lat)
-        console.log('location long',location.long)
+    useEffect(() => {
+        console.log('location', location)
+        console.log('location lat', location.lat)
+        console.log('location long', location.long)
 
 
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             console.log(position)
-            setCurrentLocation({lat:position.coords.latitude, long:position.coords.longitude})
+            setCurrentLocation({lat: position.coords.latitude, long: position.coords.longitude})
             console.log("Latitude is :", position.coords.latitude);
             console.log("Longitude is :", position.coords.longitude);
         });
-    },[location])
+    }, [location])
     console.log(currentLocation)
     console.log(location)
     return (
         <Layout>
-            <Header style={{position: 'fixed', zIndex: 1, width: '100%', height:'14%', backgroundColor:'#1E90FF', display: 'inline'} }  >
+            <Header style={{
+                position: 'fixed',
+                zIndex: 1,
+                width: '100%',
+                height: '14%',
+                backgroundColor: '#1E90FF',
+                display: 'inline'
+            }}>
                 {/*<div className="logo"/>*/}
-                <div style={{display: 'inline'}}>
-                {/*<Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>*/}
-                  {/*<Menu.Item key="1">nav 1</Menu.Item>*/}
-                  {/*<Menu.Item key="2">nav 2</Menu.Item>*/}
-                  {/*<Menu.Item key="3">nav 3</Menu.Item>*/}
-                {/*</Menu>*/}
-                {/*<GithubOutlined style={{color: "white", fontSize: 30}}/>*/}
-                    <h1 style={{color:"white"}}>{userName} </h1>
+                <div style={{display: 'flex'}}>
+                    {/*<Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>*/}
+                    {/*<Menu.Item key="1">nav 1</Menu.Item>*/}
+                    {/*<Menu.Item key="2">nav 2</Menu.Item>*/}
+                    {/*<Menu.Item key="3">nav 3</Menu.Item>*/}
+                    {/*</Menu>*/}
+                    <GithubOutlined style={{color: "white", fontSize: 30, marginTop: 25}}/>
+                    <h1 style={{color: "white", marginTop: 10, marginLeft:25}}>{userName} </h1>
+
                     {/*<div style={{paddingLeft:1200, paddingBottom:700}}>*/}
                     {/*    <Button>Log Out</Button>*/}
                     {/*</div>*/}
                 </div>
+                <div style={{float: 'right', marginTop: '-80px'}}>
+                    <Button  onClick={onLogOutHandle}>Log Out</Button>
 
+                </div>
             </Header>
-            <Content className="site-layout" style={{padding: '0 50px',paddingTop:50, marginTop: 64}}>
+            <Content className="site-layout" style={{padding: '0 50px', paddingTop: 50, marginTop: 64}}>
 
 
                 <div className="site-layout-background"
@@ -157,7 +170,7 @@ export function MapScreen(props) {
                         style={{width: 320, marginLeft: 20}}
                         enterButton
                         // name='search'
-                        type = 'search'
+                        type='search'
                         onSearch={onSearchHandle}
                     />
 
@@ -166,7 +179,7 @@ export function MapScreen(props) {
                         zoom={13}
                         style={mapStyles}
                         initialCenter={{lat: currentLocation.lat, lng: currentLocation.long}}
-                        center={{lat:location.lat, lng:location.long}}
+                        center={{lat: location.lat, lng: location.long}}
                         // center={objects.length!==0?{lat:objects[0].latitude, lng:objects[0].longitude}:{lat:21.005492, lng:105.844257}}
                     >
                         {/*<Marker*/}
@@ -178,13 +191,14 @@ export function MapScreen(props) {
                         {/*    name={'demo'}*/}
                         {/*/>*/}
                         <Marker
-                            position={{lat:currentLocation.lat,lng:currentLocation.long}}
+                            position={{lat: currentLocation.lat, lng: currentLocation.long}}
                             icon={{
                                 url: vector
                             }}
                         />
 
-                        {objects===null?undefined:objects.map((g)=><Marker position={{lat: g.latitude, lng: g.longitude}} name={g.name} onClick={onMarkerClick}/>)}
+                        {objects === null ? undefined : objects.map((g) => <Marker
+                            position={{lat: g.latitude, lng: g.longitude}} name={g.name} onClick={onMarkerClick}/>)}
 
                         <InfoWindow
                             marker={marker}
@@ -195,10 +209,7 @@ export function MapScreen(props) {
                             </div>
                         </InfoWindow>
                     </Map>
-                    <div style={{paddingLeft:15, paddingTop:550}}>
-                        <Button type="primary" onClick={onLogOutHandle}>Log Out</Button>
 
-                    </div>
                     <Modal visible={visible} onOk={handleOK} onCancel={handleCancel}>
                         <Table columns={tableColumn} dataSource={objects}/>
                     </Modal>
@@ -218,15 +229,15 @@ const mapStyles = {
 };
 const mapStateToProps = state => {
     console.log(state.object.userName)
-    return{
+    return {
         objects: state.object.objects,
-        userName : state.object.userName
+        userName: state.object.userName
     }
 }
 const mapDispatchToProps = dispatch => {
-    return{
-        getObjects: (n,lat, lng)=> dispatch({type:"API_CALL_REQUEST",name:n, lat: lat , lng: lng}),
-        resetLoginStatus: () => dispatch({type:"RESET_LOGIN_STATUS"})
+    return {
+        getObjects: (n, lat, lng) => dispatch({type: "API_CALL_REQUEST", name: n, lat: lat, lng: lng}),
+        resetLoginStatus: () => dispatch({type: "RESET_LOGIN_STATUS"})
     }
 }
 
